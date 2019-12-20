bc0b839d72f13ebebd90d6995a0d95ae6f654d9f
Phongsakorn Wisetthon <boy1556@hotmail.com>
2019-12-20 14:05:03 +0700

    wow

---
+++ work-price/work-price/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim count As Integer = 0
+        Dim price As Integer
+        price = Val(TextBox1.Text) + 150
+        While count = 0
+            If price Mod 50 = 0 Then
+                Label4.Text = "ราคาสรุป : " & price
+                count = 1
+            Else
+                price = price + 1
+                count = 0
+            End If
+        End While
+    End Sub
+End Class
---
+++ work-price/work-price/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("work-price")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("work-price")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c4f08048-0b0a-471a-9255-7b528c1a61a8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
